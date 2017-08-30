a37f4aaf1f48ee62d724f9ac838a8f438238a99a
Xavier Collantes <xcollantes@zagmail.gonzaga.edu>
2017-08-30 05:07:23 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   count = 0
 
   board.each do |element|
-    if element == '' || element == ' ' then count += 1 else continue end
+    if element == '' || element == ' ' then count += 1 else  end
   end
 
   return count
